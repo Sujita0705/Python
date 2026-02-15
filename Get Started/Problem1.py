2943069a948f7933106305a0bb323d3d941fc1f0
Sujita0705 <Sujitasingh0705@gmail.com>
2026-02-15 14:05:03 +0530

    Update Problem1.py

--- Get Started/Problem1.py
+++ Get Started/Problem1.py
@@ -1,3 +1,4 @@
+# Write a python program to print the contents of a directory using the OS module. Search online for the function which does that.
 import os
 # specify the directory you want to list
 directory_path = '/'
@@ -7,4 +8,5 @@ comtents = os.listdir(directory_path)
 
 # print each file and directory name
 for item in comtents:
+
     print(item)
