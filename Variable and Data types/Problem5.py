0ab9d78cdb412b272aee925c79729cdc21e1dd46
Sujita0705 <Sujitasingh0705@gmail.com>
2026-02-15 14:18:28 +0530

    Update Problem5.py

--- Variable and Data types/Problem5.py
+++ Variable and Data types/Problem5.py
@@ -1,4 +1,6 @@
+# Write a python program to find an average of two numbers entered by the user.
 a = int(input("Enter a value:"))
 b = int(input("Enter another value:"))
 c = (a+b)/2
+
 print("The average of the two numbers is: ", c)
