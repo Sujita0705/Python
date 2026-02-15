7e7277a7343ae8f4ce0919b15e2b29963283aa67
Sujita0705 <Sujitasingh0705@gmail.com>
2026-02-15 14:20:04 +0530

    Update Problem6.py

--- Variable and Data types/Problem6.py
+++ Variable and Data types/Problem6.py
@@ -1,6 +1,8 @@
+# Write a python program to calculate the square of a number entered by the user.
 a = int(input("Enter a value:"))
 b = a*a
 c = a**2
 # d = a^2   Using ** for exponentiation instead of ^ (which is bitwise XOR).
 print(b)
+
 print(c)
